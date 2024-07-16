7c36cab7eb3601a31c0199d6e9c6fe85882687fc
Surag <suragsudhi@gmail.com>
2024-07-16 23:34:50 +0530

    Almost integrated frontend and backend, started with dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,25 @@ import Register from './Pages/Register';
 import Login from './Pages/Login';
 import axios from 'axios';
 import {Toaster} from 'react-hot-toast'
+import { UserContextProvider} from '../context/userContext';
+import Dashboard from './Pages/Dashboard';
 
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
      <Navbar />
      <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
      <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
      </Routes>
-    </>
-  )
+    </UserContextProvider>
+  );
 }
 
 export default App
--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,10 +1,12 @@
 const User = require('../models/user')
 const {hashPassword,comparePassword} = require('../helpers/auth')
+const jwt = require('jsonwebtoken');
 
 const test =(req,res) => {
     res.json('test is working')
-}
+};
 
+//Register Endpoint
 const registerUser = async (req,res) => {
    try {
       const {name, email, password} = req.body;
@@ -40,9 +42,53 @@ const registerUser = async (req,res) => {
    } catch (error) {
       console.log(error)
    }
+};
+
+//Login Endpoint
+const loginUser = async (req, res) => {
+ try {
+    const {email,password} = req.body;
+
+    //Check if user exists
+    const user = await User.findOne({email});
+    if(!user) {
+        return res.json({
+            error: 'No user found'
+        })
+    }
+    // Check if passwords match
+    const match = await comparePassword(password,user.password)
+    if(match) {
+     jwt.sign({email: user.email, id: user._id, name: user.name},process.env.JWT_SECRET, {}, (err, token) => {
+        if(err) throw err;
+        res.cookie('token',token).json(user)
+     })
+    }
+    if(!match) {
+        res.json({
+            error: 'Password do not match'
+        })
+    }
+ } catch (error) {
+    console.log(error)
+ }
+}
+
+const getProfile = (req, res) => {
+ const {token} = req.cookies
+ if(token) {
+    jwt.verify(token, process.env.JWT_SECRET, {},(err, user) => {
+        if(err) throw err;
+        res.json(user)
+    })
+ } else {
+    res.json(null)
+ }
 }
 
 module.exports = {
     test,
-    registerUser
-}
+    registerUser,
+    loginUser,
+    getProfile
+};
